b4ecc1011fdfb6a71e6a6d21c43f1dec1d3d4de1
Isaac-moore <alabisheyi2001@yahoo.com>
2017-08-20 21:48:36 +0100

    Update usecolumns.php

--- predictgcse/usecolumns.php
+++ predictgcse/usecolumns.php
@@ -1,9 +1,9 @@
 <?php
 
-   $host        = "host = ec2-176-34-113-15.eu-west-1.compute.amazonaws.com";
+   $host        = "host = ";
    $port        = "port = 5432";
-   $dbname      = "dbname = d4on6pdtb6ivkq";
-   $credentials = "user = enetzncoupodfd password=e7adb8885773bea9ff108fce3755858014e70d487b8476e79ea33671c14ce35d";
+   $dbname      = "dbname = ";
+   $credentials = "user =  password=";
 
    $db = pg_connect( "$host $port $dbname $credentials"  );
    if(!$db) {
