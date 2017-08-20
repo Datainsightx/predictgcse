9061a73f024a52dc860d7e9a24a2a3c7662c7d4a
Isaac-moore <alabisheyi2001@yahoo.com>
2017-08-20 21:46:29 +0100

    Update upload.php

--- predictgcse/upload.php
+++ predictgcse/upload.php
@@ -2,11 +2,10 @@
 
 $target_dir = "/Upload";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-
-$host = "host = ec2-176-34-113-15.eu-west-1.compute.amazonaws.com";
+$host = "host=";
 $port = "port = 5432";
-$dbname = "dbname = d4on6pdtb6ivkq";
-$credentials = "user = enetzncoupodfd password=e7adb8885773bea9ff108fce3755858014e70d487b8476e79ea33671c14ce35d";
+$dbname = "dbname = ";
+$credentials = "user =  password=";
 
 $db = pg_connect( "$host $port $dbname $credentials"  );
 if(!$db) {
@@ -16,11 +15,10 @@ if(!$db) {
       echo "Opened database successfully\n";
    }
 
-$query = "INSERT INTO school_data VALUES ('$_POST[schoolid]','$_POST[target_file]',  
-'$_POST[price]','$_POST[dop]')";  
+$query = "INSERT INTO school_data VALUES ('$_POST[schoolid]','$_POST[target_file]') 
 $result = pg_query($query); 
 $test_data = pg_fetch_all($result);
 
-$fieldname = pg_field_name($test_data);//names of columns
+$fieldname = pg_field_name($test_data);
 
 ?>
